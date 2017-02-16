65a8b500ee42ea89dd8613b173bd6b1bb363880d
abbet <abbet.christian@gmail.com>
2017-02-17 00:28:02 +0100

    Celetrial object

---
+++ Revolution/Assets/Scripts/CelestialObject.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CelestialObject : MonoBehaviour
+{
+
+    private bool isFixed = true;
+    private Rigidbody2D m_Body;
+
+    // Use this for initialization
+	void Start () {
+
+	}
+	
+	void Update () {
+
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        Debug.Log("test");
+    }
+
+
+    public void Init(bool isFixed, Vector2 velocity)
+    {
+        m_Body = this.GetComponent<Rigidbody2D>();
+        SetIsFixed(isFixed);
+        SetVelocity(velocity);
+
+    }
+
+    public double GetMass()
+    {
+        return m_Body.mass;
+    }
+
+    public void SetVelocity(Vector2 velocity)
+    {
+        if(m_Body != null)
+            m_Body.velocity = velocity;
+    }
+
+    public void SetMass(float mass)
+    {
+        if(m_Body != null)
+            m_Body.mass = mass;
+    }
+
+    public void SetIsFixed(bool isFixed)
+    {
+        this.isFixed = isFixed;
+    }
+
+    public bool GetIsFixed()
+    {
+        return isFixed;
+    }
+
+    private Vector2 GetPosition()
+    {
+        return m_Body != null ? m_Body.position : new Vector2(0, 0);
+    }
+
+    public void AddForceCelestial(List<GameObject> list_Objects)
+    {
+        if (m_Body == null || list_Objects == null) return;
+
+        foreach (var go in list_Objects)
+        {
+            if (go == null) continue;
+
+            var cel = go.GetComponent<CelestialObject>();
+            if (cel != this && !isFixed)
+            {
+                m_Body.AddForce(GetForceInteraction(this, cel));
+            }
+        }
+    }
+
+    private static Vector2 GetForceInteraction(CelestialObject obj1, CelestialObject obj2)
+    {
+        var r = obj2.GetPosition() - obj1.GetPosition();
+        var m1 = (float)obj1.GetMass();
+        var m2 = (float)obj2.GetMass();
+        if (r.magnitude < 1)
+            r = r.normalized;
+        return r.normalized * (( m1 * m2) / (float)Math.Pow(r.magnitude,2));
+    }
+
+}
---
+++ Revolution/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject prefabBlackHole;
+    public GameObject prefabSatellite;
+
+    private readonly List<GameObject> celestialObjects = new List<GameObject>();
+
+    // Use this for initialization
+	void Start () {
+	    StartCoroutine(GameLoop());
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        Debug.Log("tesst");
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	    // Update forces
+	    foreach (var go in celestialObjects)
+	    {
+	        if (go == null) continue;
+
+	        var cel = go.GetComponent<CelestialObject>();
+	        if (cel != null && !cel.GetIsFixed())
+	        {
+	            cel.AddForceCelestial(celestialObjects);
+	        }
+	    }
+
+	}
+
+    private IEnumerator GameLoop()
+    {
+        yield return StartCoroutine(InitPlaying());
+        yield return StartCoroutine(RoundPlaying());
+    }
+
+    private IEnumerator InitPlaying()
+    {
+
+        celestialObjects.Add((GameObject) Instantiate(prefabSatellite, new Vector3(-1, 1, 0), Quaternion.identity));
+        celestialObjects.Add((GameObject) Instantiate(prefabSatellite, new Vector3(1, 1, 0), Quaternion.identity));
+        celestialObjects.Add((GameObject) Instantiate(prefabBlackHole, new Vector3(8, 0, 0), Quaternion.identity));
+        celestialObjects.Add((GameObject) Instantiate(prefabBlackHole, new Vector3(-8, 0, 0), Quaternion.identity));
+
+        celestialObjects[0].GetComponent<CelestialObject>().Init(false, new Vector2(-0.2f, 3));
+        celestialObjects[1].GetComponent<CelestialObject>().Init(false, new Vector2(3.2f, 0));
+        celestialObjects[2].GetComponent<CelestialObject>().Init(true, new Vector2(0, 0));
+        celestialObjects[3].GetComponent<CelestialObject>().Init(true, new Vector2(0, 0));
+
+        yield return null;
+    }
+
+    private IEnumerator RoundPlaying()
+    {
+        while (true)
+        {
+
+            yield return null;
+        }
+    }
+
+}
